3276deedc08e6d7d40a0ced66ed0b263d4fdb63b
Chandra Sekhar Mullu <47244469+iamknownstranger@users.noreply.github.com>
2023-09-11 00:19:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,31 @@
 import nltk
-nltk.download('punkt')
+nltk.download('all')
 
-import streamlit as st
+import os
+
+import openai
 import requests
+import streamlit as st
+from newspaper import Article
 
 # TODO: Add summary and description to the story
 
-# # Import the library
-from newspaper import Article
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Function to fetch Hacker News stories
+# Function to fetch Hacker News stories through the official API
 def fetch_hacker_news_stories():
     url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
     response = requests.get(url)
-    top_story_ids = response.json()    
+    top_story_ids = response.json()[:5]
     return top_story_ids
 
+
+@st.cache_data()
 def get_story(story_id):
     url = f"https://hacker-news.firebaseio.com/v0/item/{story_id}.json?print=pretty"
     response = requests.get(url)
     story = response.json()
-    # st.write(f"**Title:** [{story['title']}]({story['url']})")
 
     article = Article(story["url"])
 
@@ -33,10 +37,18 @@ def get_story(story_id):
     article.nlp()
 
     # Print the article title
-    story["TITLE"] = article.title
+    story["title"] = article.title
 
-    # Print the article summary
-    story["SUMMARY"] = article.summary
+    # Geneate summary of the article using GPT-3
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k-0613",
+        messages=[
+            {"role": "system", "content": f"You're a great research article writer! and very skillful in summarizing articles. I have a task for you."},
+            {"role": "user", "content": f"summarize the followning article briefly and use markdown formatting wherever needed like code snippets with headers less than two stars: {article.text}"},
+        ],
+    )
+    summary = response["choices"][0]["message"]["content"]
+    story["summary"] = summary
 
     return story
 
@@ -51,11 +63,10 @@ def main():
         with st.container():
             story = get_story(story_id)
             st.subheader(f"[{story['title']}]({story['url']})")
-            st.write(f"**Author:** {story['by']}")
-            st.write(f"**Score:** {story['score']}")
-            st.write(f"**Comments:** {story['descendants']}")
-            st.write(f"**Summary:** {story['SUMMARY']}")
+            st.write(f"*Author:* {story['by']}")
+            st.write(f"**Summary:** {story['summary']}")
             st.write("---")
+
+
 if __name__ == "__main__":
     main()
-
